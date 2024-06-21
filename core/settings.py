a445b76c02cf5254d961fb60295d6c7d72a20be3
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-21 08:10:00 +0545

    Initial

--- core/settings.py
+++ core/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["Templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
