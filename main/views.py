31b792f8e3ff107142b29f8efdfa540b4ce3f4bd
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-22 19:56:42 +0545

    404.html and Index.html Created

--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Home(request):
+    if request.method=="POST":
+        pass
+    return render(request,"index.html")
+
+
+
+
+def Custom404(request):
+    return render(request,"404.html")
+
+
