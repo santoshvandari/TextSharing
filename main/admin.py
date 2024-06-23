5cd54a34f4be319845318834ea1faf1c7ed50c2b
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-23 11:14:27 +0545

    modified:   main/admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from main.models import SharedText
 
 # Register your models here.
+class SharedTextAdmin(admin.ModelAdmin):
+    list_display = ('title','note','slug', 'fileid', 'upload_time', 'expiration_time', 'is_expired')
+    list_filter = ('upload_time', 'expiration_time')
+    search_fields = ('title', 'slug', 'fileid')
+    ordering = ('upload_time', 'expiration_time')
+
+admin.site.register(SharedText,SharedTextAdmin)
