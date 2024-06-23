9e567efa2b82dab11356dddfeb98ebc5d8ddd65e
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-23 10:50:33 +0545

    Initial

--- main/views.py
+++ main/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
     if request.method=="POST":
-        pass
+        notetitle=request.POST.get('NoteTitle')
+        notecontent=request.POST.get("NoteContent")
     return render(request,"index.html")
 
 
