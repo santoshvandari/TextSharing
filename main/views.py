cb2102a7bee9704607eb03c6e0395830954fb916
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-23 10:56:50 +0545

    Initial

--- main/views.py
+++ main/views.py
@@ -3,8 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
     if request.method=="POST":
-        notetitle=request.POST.get('NoteTitle')
-        notecontent=request.POST.get("NoteContent")
+        notetitle=request.POST.get('NoteTitle').strip()
+        notecontent=request.POST.get("NoteContent").strip()
+        if notetitle and notecontent:
+            print(notetitle,notecontent)
+            statusdata={
+                'status':'success',
+                'slug':'http://127.0.0.1:8000/d/'+slug,
+                'fileid':fileno
+            }
+
+
+        else:
+            successdata={
+                'status':'error',
+                'message':'All Fields are Required.'
+            }
+            return render(request,'index.html',successdata)
     return render(request,"index.html")
 
 
