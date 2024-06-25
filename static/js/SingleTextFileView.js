580da6281877e64aa3f9ac23e389e4b1946ef62e
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-25 19:12:11 +0545

    Copy Feature Added

--- static/js/SingleTextFileView.js
+++ static/js/SingleTextFileView.js
@@ -2,6 +2,4 @@ const copyBtn=document.getElementById("copy");
 const noteContent=document.getElementById("NoteContent");
 copyBtn.addEventListener("click",()=>{
     window.navigator.clipboard.writeText(noteContent.value);
-    console.log("btnclicked")
-    console.log(noteContent.value)
 })
